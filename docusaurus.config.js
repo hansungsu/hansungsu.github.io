f89a83e61f6120401f9c03893e03b1a3b605ce8c
HOTSAUCE <38658225+hansungsu@users.noreply.github.com>
2024-08-22 04:42:21 +0000

    docusaurus config setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,14 +8,14 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "HOTSAUCE108",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
-  url: "https://endiliey.github.io", // Your website URL
+  url: "https://github.com/hansungsu", // Your website URL
   baseUrl: "/",
-  projectName: "endiliey.github.io",
-  organizationName: "endiliey",
+  projectName: "hansungsu.github.io",
+  organizationName: "hansungsu",
   trailingSlash: false,
 
   onBrokenLinks: "throw",
@@ -61,7 +61,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "HOTSAUCE SITE",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
